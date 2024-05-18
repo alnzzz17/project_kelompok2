d7d5ce532c4bd8331de66d557bc3921e9b3ae770
Erllyta Rachma <142652234+alnzzz17@users.noreply.github.com>
2024-05-18 20:01:44 +0700

    Add files via upload

---
+++ routes/appointment.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    getAllAppoint,
+    addAppoint,
+    deleteAppoint,
+    editAppointDetail,
+    getAppointById,
+    getAppointByPasien,
+    getAppointByDokter,
+    getAppointByPoli
+} = require("../controller/appointment");
+
+//GET ALL APPOINTMENT
+router.get("/appointments/fetch-all", getAllAppoint);
+
+//ADD NEW APPOINTMENT
+router.post("/appointments/add-new", addAppoint);
+
+//DELETE APPOINTMENT
+router.delete("/appointments/:appId", deleteAppoint);
+
+//EDIT APPOINTMENT DETAILS
+router.put("/appointments/edit", editAppointDetail);
+
+//GET APPOINTMENT BY ID
+router.get("/appointments/:appId", getAppointById);
+
+//GET APPOINTMENT BY PASIEN ID
+router.post("/appointments/:idPasien", getAppointByPasien);
+
+//GET APPOINTMENT BY DOKTER ID
+router.post("/appointments/:idDokter", getAppointByDokter);
+
+//GET APPOINTMENT BY POLI
+router.post("/appointments/poli", getAppointByPoli);
+
+module.exports = router;
