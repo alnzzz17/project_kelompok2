86d6a5ec0673e3f0aaf90ae08393eda52727bdc2
Erllyta Rachma <142652234+alnzzz17@users.noreply.github.com>
2024-05-22 15:25:10 +0700

    update upload_file.js

--- middleware/upload_file.js
+++ middleware/upload_file.js
@@ -2,13 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // // kalau error lakukan sesuatau
-    // if (error) {
-    //   cb("error saat mengupload file");
-    // } else {
-    // kondisi ketika sukses
     cb(null, "files");
-    //  
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
