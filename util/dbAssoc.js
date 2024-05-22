ec9b72b662f5c228c20bc48a837f1b0048edf421
Erllyta Rachma <142652234+alnzzz17@users.noreply.github.com>
2024-05-22 15:33:18 +0700

    Update dbAssoc.js

--- util/dbAssoc.js
+++ util/dbAssoc.js
@@ -1,7 +1,26 @@
+require("dotenv").config();
 const sequelize = require("./db_connect");
-const { Resepsionis, Dokter, Pasien } = require('../model/User'); //Import models
-
+const bcrypt = require('bcrypt');
+const { Resepsionis, Dokter, Pasien } = require('../model/User');
 const Appointment = require('../model/Appointment');
+const Poli = require('../model/Poli');
+
+const poli = [
+  {nama: "UMUM"},
+  {nama: "GIGI"}
+]
+
+const adminPassword = process.env.ADMIN_PWD;
+const hashedPwd = bcrypt.hashSync(adminPassword, 5);
+
+const admin = {
+  idRsp: process.env.ADMIN_ID,
+  fullName: process.env.ADMIN_FULLNAME,
+  password: hashedPwd,
+  email: process.env.ADMIN_EMAIL,
+  phoneNumber: process.env.ADMIN_PHONE,
+  role: "Admin"
+}
 
 Pasien.hasMany(Appointment, { foreignKey: 'idPasien' });
 Appointment.belongsTo(Pasien, { foreignKey: 'idPasien' });
@@ -12,12 +31,19 @@ Appointment.belongsTo(Dokter, { foreignKey: 'idDokter' });
 Resepsionis.hasMany(Appointment, { foreignKey: 'idResepsionis' });
 Appointment.belongsTo(Resepsionis, { foreignKey: 'idResepsionis' });
 
+Poli.hasMany(Appointment, { foreignKey: 'idPoli' });
+Appointment.belongsTo(Poli, { foreignKey: 'idPoli' });
+
+Poli.hasMany(Dokter, { foreignKey: 'idPoli' });
+Dokter.belongsTo(Poli, { foreignKey: 'idPoli' });
+
 const association = async()=>{
   try {
     await sequelize.sync({force: false});
-    // Dokter.bulkCreate();
+    // Poli.bulkCreate(poli);
+    // await Dokter.create();
+    //await Resepsionis.create(admin);
     // await Pasien.create();
-    // await Resepsionis.create();
     // await Appointment.create();
   } catch (error) {
     console.log(error.message);
